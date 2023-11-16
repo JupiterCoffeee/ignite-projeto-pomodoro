faaad4864b3e28a31b35142d8749e04a50eb6dac
JupiterCoffeee <jvcfialho@gmail.com>
2023-11-16 15:16:43 +0000

    Components Props Drilling

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,40 @@
+import { FormContainer, MinutesInput, TaskInput } from "./style";
+import { useContext } from "react";
+import { CyclesContext } from "../..";
+import { useFormContext } from "react-hook-form";
+
+export function NewCycleForm() {
+    const { activeCycle } = useContext(CyclesContext)
+    const { register } = useFormContext()
+    
+    return (
+        <FormContainer>
+            <label htmlFor="">Vou trabalhar em</label>
+            <TaskInput 
+                type="text" 
+                id="task" 
+                placeholder='Dê um nome para o seu projeto'
+                list='task-suggestions'
+                disabled={!!activeCycle}
+                {...register('task')}
+            />
+
+            <datalist id='task-suggestions'>
+                <option value="Projeto 1"/>
+            </datalist>
+
+            <label htmlFor="">durante</label>
+            <MinutesInput 
+                type="number" 
+                id="minutesAmount" 
+                step={5}
+                min={5}
+                max={60}
+                disabled={!!activeCycle}
+                {...register('minutesAmount', {valueAsNumber: true})}
+            />
+
+            <span>minutos.</span>
+        </FormContainer>
+    )
+}
